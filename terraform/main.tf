1c8321ecca0fd857497483e134dc0d1b9af2de57
mehrankhantt <cloud.mehrankhan@gmail.com>
2025-08-27 15:01:07 +0500

    improvedsxs

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,12 @@
 terraform {
+
+  backend "s3" {
+    bucket         = "your-terraform-state-bucket"
+    key            = "path/to/your/state.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks" # Optional but recommended for state locking
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
